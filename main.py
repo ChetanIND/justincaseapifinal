f584e931c027b262ed391d2a7cb3737b60d03a24
ChetanIND <chetan.indulkar007@gmail.com>
2024-02-22 18:16:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ async def predict(user_id: str = Path(..., title="The ID of the user to predict 
 
 if __name__ == "__main__":
     port = int(getenv("PORT", 8000))
-    uvicorn.run("main:app", host="127.0.0.1", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
 
 
 
